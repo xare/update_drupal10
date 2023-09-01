f841b603c50048552f7cc25bff3e88d5d364433c
xare <xaresd@gmail.com>
2023-09-01 15:37:40 +0200

    Correct 2 col fluid node actividad full template

--- export_lapanterarossa_db.sh
+++ export_lapanterarossa_db.sh
@@ -13,6 +13,8 @@ declare -A DIRS=(
 
 #Localcal MySQL Variables
 SSH_DB_PATH="$REMOTE_PATH/db/"
+SSH_UPSTREAM_PATH="$SSH_DB_PATH/upstream/"
+SSH_DOWNSTREAM_PATH="$SSH_DB_PATH/downstream/"
 
 #Dump file parameters
 LOCAL_BACKUP_PATH="$LOCAL_PATH/db"
@@ -20,31 +22,31 @@ LOCAL_BACKUP_PATH="$LOCAL_PATH/db"
 # 1. Dump the remote specified tables
 REMOTE_DUMP_NAME="remote_extract_$(date +'%d_%m_%Y_%H%M').sql"
 ssh $SSH_USER@$SSH_SERVER <<EOF
-    mysqldump -u $REMOTE_DB_USER -p$REMOTE_DB_PASS -h $REMOTE_DB_HOST $REMOTE_DB_NAME commerce_order commerce_order_item commerce_order_item__adjustments commerce_order__adjustments commerce_order__coupons commerce_order__order_items users > $SSH_DB_PATH/$REMOTE_DUMP_NAME
+    mysqldump -u $REMOTE_DB_USER -p$REMOTE_DB_PASS -h $REMOTE_DB_HOST $REMOTE_DB_NAME commerce_order commerce_order_item commerce_order_item__adjustments commerce_order__adjustments commerce_order__coupons commerce_order__order_items users > $SSH_DOWNSTREAM_PATH/$REMOTE_DUMP_NAME
 EOF
 
 # 2. Download the remote extract to local environment
-rsync -avz $SSH_USER@$SSH_SERVER:$SSH_DB_PATH/$REMOTE_DUMP_NAME $LOCAL_BACKUP_PATH/
+rsync -avz $SSH_USER@$SSH_SERVER:$SSH_DOWNSTREAM_PATH/$REMOTE_DUMP_NAME $LOCAL_BACKUP_PATH/
 
 # 3. Insert the downloaded tables' data into your local database
 mysql $LOCAL_DB_NAME < $LOCAL_BACKUP_PATH/$REMOTE_DUMP_NAME
 
 # 4. Dump and sync your entire local database (now with downloaded tables) to the remote
-FILENAME="$LOCAL_DB_NAME_$(date +'%d_%m_%Y_%H%M').sql"
+FILENAME="$LOCAL_DB_NAME"_"$(date +'%d_%m_%Y_%H%M').sql"
 
 # Dump the database
 mysqldump $LOCAL_DB_NAME > $LOCAL_BACKUP_PATH/$FILENAME
 echo "DB saved at $LOCAL_BACKUP_PATH/$FILENAME"
 
 # Sync the dump file to the remote server
-rsync -avz $LOCAL_BACKUP_PATH/$FILENAME $SSH_USER@$SSH_SERVER:$SSH_DB_PATH
+rsync -avz $LOCAL_BACKUP_PATH/$FILENAME $SSH_USER@$SSH_SERVER:$SSH_UPSTREAM_PATH
 
 # Connect to remote server and create the database if it doesn't exist, then import the dump
 ssh $SSH_USER@$SSH_SERVER <<EOF
-    LATEST_DUMP=\$(ls -Art $SSH_DB_PATH | tail -n 1)
-    echo "We are updating the database on the remote server"
+    LATEST_DUMP=\$(ls -Art $SSH_UPSTREAM_PATH | tail -n 1)
+    echo "We are updating the database on the remote server: $LATEST_DUMP"
     mysql -u $REMOTE_DB_USER -p$REMOTE_DB_PASS -h $REMOTE_DB_HOST -e "CREATE DATABASE IF NOT EXISTS $REMOTE_DB_NAME;"
-    mysql -u $REMOTE_DB_USER -p$REMOTE_DB_PASS -h $REMOTE_DB_HOST $REMOTE_DB_NAME < $SSH_DB_PATH/\$LATEST_DUMP
+    mysql -u $REMOTE_DB_USER -p$REMOTE_DB_PASS -h $REMOTE_DB_HOST $REMOTE_DB_NAME < $SSH_UPSTREAM_PATH/\$LATEST_DUMP
 EOF
 
 
